9c58f7901204f625e6c714a3b0d5778f4c235aba
saerothh <saeroth@gmail.com>
2025-01-28 19:20:42 +0000

    Actualización del script .sh con nuevos cambios

--- airdcpp-install.sh
+++ airdcpp-install.sh
@@ -1,42 +1,41 @@
 #!/usr/bin/env bash
 
-$STD apt-get update
-$STD apt-get upgrade -y
+apt-get update
+apt-get upgrade -y
 
-$STD apt-get install -y curl
-$STD apt-get install -y sudo
-$STD apt-get install -y mc
+apt-get install -y curl sudo mc
 
 mkdir -p /var/lib/airdcpp/
 chmod 775 /var/lib/airdcpp/
-$STD wget --content-disposition 'https://web-builds.airdcpp.net/stable/airdcpp_latest_master_64-bit_portable.tar.gz'
-$STD tar -xvzf airdcpp_latest_master.*.tar.gz
+
+wget --content-disposition 'https://web-builds.airdcpp.net/stable/airdcpp_latest_master_64-bit_portable.tar.gz'
+tar -xvzf airdcpp_latest_master.*.tar.gz
 mv airdcpp-webclient /opt
 chmod 775 /opt/airdcpp-webclient
 
-cat </etc/systemd/system/airdcpp.service
+cat <<EOF > /etc/systemd/system/airdcpp.service
 [Unit]
 Description=Airdcpp Daemon
 After=syslog.target network.target
+
 [Service]
 UMask=0002
 Type=simple
 ExecStart=/opt/airdcpp-webclient/airdcppd
-WorkingDirectory=/opt/airdcpp-webclien
+WorkingDirectory=/opt/airdcpp-webclient
 TimeoutStopSec=20
 KillMode=process
 Restart=on-failure
+
 [Install]
 WantedBy=multi-user.target
 EOF
 
-systemctl -q daemon-reload
-systemctl enable --now -q airdcpp
+systemctl daemon-reload
+systemctl enable --now airdcpp
 
-motd_ssh
-customize
-
+# Limpieza final
 rm -rf airdcpp_latest_master.*.tar.gz
-$STD apt-get -y autoremove
-$STD apt-get -y autoclean
+apt-get -y autoremove
+apt-get -y autoclean
 
