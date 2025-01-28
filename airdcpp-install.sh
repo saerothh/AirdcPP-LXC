5c1011ebf4073aae5d40721659bebf1190e6e7f2
saerothh <saeroth@gmail.com>
2025-01-28 19:44:31 +0000

    Actualización del script .sh con nuevos cambios

--- airdcpp-install.sh
+++ airdcpp-install.sh
@@ -33,6 +33,9 @@ fi
 mv airdcpp-webclient /opt
 chmod 775 /opt/airdcpp-webclient
 
+#Ejecucion de Airdcpp por primera vez
+./opt/airdcpp-webclient/airdcppd --configure
+
 # Crear el archivo del servicio systemd
 cat <<EOF > /etc/systemd/system/airdcpp.service
 [Unit]
